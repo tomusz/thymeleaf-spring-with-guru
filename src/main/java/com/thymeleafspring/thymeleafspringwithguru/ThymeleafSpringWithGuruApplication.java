7973d920d121592c83308687ec0ecab1ca285a7b
Tomasz Wachowicz <wachowicztomek@gmail.com>
2018-08-22 00:49:29 +0200

    1st commit

---
+++ src/main/java/com/thymeleafspring/thymeleafspringwithguru/ThymeleafSpringWithGuruApplication.java
@@ -0,0 +1,12 @@
+package com.thymeleafspring.thymeleafspringwithguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringWithGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringWithGuruApplication.class, args);
+	}
+}
