ce82419936ef2629bbb0d0a60406b296eb03e27c
Tomasz Wachowicz <wachowicztomek@gmail.com>
2018-08-31 08:03:21 +0200

    adding missing files problem to solve iteration of categories

--- src/test/java/com/thymeleafspring/thymeleafspringwithguru/ThymeleafSpringWithGuruApplicationTests.java
+++ src/test/java/com/thymeleafspring/thymeleafspringwithguru/ThymeleafSpringWithGuruApplicationTests.java
@@ -1,5 +1,6 @@
 package com.thymeleafspring.thymeleafspringwithguru;
 
+import com.thymeleafspring.thymeleafspringwithguru.services.ProductServiceImp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
